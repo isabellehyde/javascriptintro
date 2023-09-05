9bcdad10e002506bfeceb81ff33bc37a1a484733
Belle Hyde <LittleRedwood@Belles-MacBook-Pro.local>
2023-09-05 11:37:20 -0400

    added files

---
+++ java.js
@@ -0,0 +1,6 @@
+var firstName = "Isabelle"
+var lastName = "Hyde"
+var age = 24
+alert ("my name is " + firstName + " " + lastName);
+alert ("I am " + age + " years old")
+console.log ("I am bonkers for this class")
